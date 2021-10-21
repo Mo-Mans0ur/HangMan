5cdcec830226c866f357150dcbc0f91e6acc146d
Mo-Mans0ur <Moha1998@outlook.dk>
2021-10-21 15:39:42 +0200

    Getting Started

---
+++ src/FileReader.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileReader {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static File randomWords = new File("Resource/randomWords.csv");
+    static ArrayList<>
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("welcome to Hangman ");
         System.out.println("______________________________________");
 
+        ifFoundFile();
+
+    }
+
+    public static void ifFoundFile() {
+
+        int counter = 0;
+        try {
+            Scanner scanner = new Scanner(randomWords);
+
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (counter != 0) {
+
+
+
+
+
+
+                }
+                counter++;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file could not be found");
+            e.printStackTrace();
+        }
 
     }
 }
